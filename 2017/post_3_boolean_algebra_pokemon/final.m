cdd1cb157075f515181a3d81c30f318b78420f8a
dinal herath <dinal.bing@gmail.com>
2017-07-21 22:32:58 +0530

    post 3: material work in progress

--- 2017/post_3_boolean_algebra_pokemon/final.m
+++ 2017/post_3_boolean_algebra_pokemon/final.m
@@ -11,6 +11,16 @@ back_r = back(:,:,1);
 back_g = back(:,:,2);
 back_b = back(:,:,3);
 
+figure;
+subplot(241);imshow(man);title('charachter (original)');
+subplot(242);imshow(man_r);title('charachter (channel R)');
+subplot(243);imshow(man_g);title('charachter (channel G)');
+subplot(244);imshow(man_b);title('charachter (channel B)');
+subplot(245);imshow(back);title('background (original)');
+subplot(246);imshow(back_r);title('background (channel R)');
+subplot(247);imshow(back_g);title('background (channel G)');
+subplot(248);imshow(back_b);title('background (channel B)');
+
 temp_r = combine_image(man_r,back_r);
 temp_g = combine_image(man_g,back_g);
 temp_b = combine_image(man_b,back_b);
@@ -27,3 +37,8 @@ final_image(:,:,3)= temp_b;
 
 figure;
 imshow(final_image);
+
+% figure;% the images for front matter 
+% subplot(131);imshow(back);
+% subplot(132);imshow(man);
+% subplot(133);imshow(final_image);
