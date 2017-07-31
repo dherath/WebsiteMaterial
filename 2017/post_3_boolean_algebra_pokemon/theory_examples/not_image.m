f809702a7c022f93b3cc7b72335b95e400149cb0
dinal herath <dinal.bing@gmail.com>
2017-07-31 08:07:13 +0530

    post 3: theory not image example

--- 2017/post_3_boolean_algebra_pokemon/theory_examples/not_image.m
+++ 2017/post_3_boolean_algebra_pokemon/theory_examples/not_image.m
@@ -5,5 +5,5 @@ im1 = imread('circles.png');
 im2 = not(im1);
 
 figure;
-subplot(121);imshow(im1);title('original image(Im1)');
-subplot(122);imshow(im2);title('NOT(Im1)');
+subplot(121);imshow(im1);title('input');
+subplot(122);imshow(im2);title('output');
